dd8497d795e659c1de5b3be940a78fce8f24efac
alu53381650f <alu53381650f@ti4-12>
2015-03-16 20:19:12 +0100

    16 marz 15

---
+++ PRO_T10/src/exercises/Main.java
@@ -0,0 +1,18 @@
+package exercises;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(StringMatcher.method1("Yes"));
+		System.out.println(StringMatcher.method2("This is true"));
+		System.out.println(StringMatcher.method3("OLE"));
+		System.out.println(StringMatcher.method4("amen"));
+		System.out.println(StringMatcher.method5("amen"));
+		System.out.println(StringMatcher.method6("amen299"));
+		System.out.println(StringMatcher.method7("255.255.255.255"));
+
+		
+
+	}
+	
+}
